b09891451e3b6cf74449198769c3b6f45285e098
Josiah <63004866+Necl0@users.noreply.github.com>
2022-12-21 23:15:38 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
 import discord
 import re
 import requests
+import random
+import asyncio
+import json
+
+from discord.ext import tasks
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-
 
 def get_poke(name):
     url = f'https://pokeapi.co/api/v2/pokemon/{name}'
@@ -19,6 +23,7 @@ def get_poke(name):
 @client.event
 async def on_ready():
     print(f'Entering world as {client.user}')
+    spawner.start()
 
 
 @client.event
@@ -26,8 +31,17 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.content.startswith('!setspawn'):
+        id = message.content.split(' ')[1]
+        channel = client.get_channel(id)
+        # dump the id into json file
+        with open('channel.json', 'w') as f:
+            json.dump({'id': id}, f)
 
-    if message.content.startswith('!p'):
+        await message.channel.send('Spawn channel set!')
+
+
+    elif message.content.startswith('!p'):
         name = message.content.split(' ')[1].lower().strip()
 
         p_json = get_poke(name)
@@ -79,7 +93,7 @@ async def on_message(message):
                     embed.color = discord.Color.dark_grey()
                 elif match.group(1) == 'steel':
                     embed.color = discord.Color.dark_grey()
-            
+
             embed.add_field(name="Stats", value=f"""
                                                 HP: {p_json['stats'][0]['base_stat']}
                                                 Attack: {p_json['stats'][1]['base_stat']}
@@ -91,23 +105,58 @@ async def on_message(message):
             embed.add_field(name="Abilities", value='\n'.join([a['ability']['name'].title() for a in p_json['abilities']]), inline=False)
             embed.add_field(name="Moves", value='\n'.join([m['move']['name'].title() for m in p_json['moves'][:5]]), inline=False)
 
-
             embed.set_image(url=sprite)
+
             embed.set_footer(text="Made by @Neclo#5545")
             await message.channel.send(embed=embed)
 
             return
 
     elif message.content.startswith('!help'):
-        await message.channel.send('!p <pokemon name>')
+        await message.channel.send(""""!p <pokemon name> - get info on a pokemon
+            !setspawn <channel id> - set the channel to spawn pokemon in
+            !help - get help""")
+
     elif message.content.startswith('!about'):
         await message.channel.send('Pokedex Discord bot.\nMade by Neclo#5545')
     elif message.content.startswith('!invite'):
         await message.channel.send('https://discord.com/api/oauth2/authorize?client_id=1053883092684783748&permissions=8&scope=bot')
     elif message.content.startswith('!github'):
         await message.channel.send('https://github.com/Necl0/pokedex')
+    elif message.content.startswith('!spawn'):
+        await poke_spawn()
 
-client.run('token')
+
+async def poke_spawn():
+    p_json = get_poke(random.randint(1, 898))
+
+    # get the channel to send the message to
+    with open('channel.json', 'r') as f:
+        channel_id = json.load(f)['id']
+
+    channel = client.get_channel(int(channel_id))
+    poke_embed = discord.Embed(title=p_json['name'].title(), description="A wild pokemon has spawned! Type !p <pokemon name> to catch it!")
+    poke_embed.set_image(url=p_json['sprites']['front_default'])
+    poke_embed.set_footer(text="Made by @Neclo#5545")
+
+    await channel.send(embed=poke_embed)
+    await asyncio.sleep(5)
+    await channel.send(f"The pokemon disappeared. It's a {p_json['name'].title()}!")
+
+@tasks.loop(seconds=10)
+async def spawner():
+    with open('channel.json', 'r') as f:
+        channel_id = json.load(f)['id']
+
+    channel = client.get_channel(int(channel_id))
+    chance = random.randint(1, 100)
+    if chance == 1:
+        await poke_spawn()
+
+
+
+
+client.run('MTA1Mzg4MzA5MjY4NDc4Mzc0OA.GNrBg6.Yj2Av5LkPG5aTQTo7cLHALBmfd3nHQla8XE1Zk')
 
 
 
